72a3aadcd4d901563559cfa7ef67b5c429686ca0
sapradhan <sapradhan@users.noreply.github.com>
2025-04-21 10:36:16 +0545

    filter ngrams of interest limit to devnagari characters only.

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ stats for improving android keyboard layout.
 generated char ngrams of length 1-3 in `nep-np_web_2016_100K/nep-np_web_2016_100K-ngram.tsv`
 
 Corpus source: [Web - 2016 100k](https://wortschatz.uni-leipzig.de/en/download/Nepali)
-
+Postprocess: 
+- replace र्‍ [u0930 u094D u200D] with ऱ् [u0931 u094D]: Eyelash half RA (as in पऱ्यो) 
--- ngram-char.py
+++ ngram-char.py
@@ -1,12 +1,18 @@
 from sklearn.feature_extraction.text import CountVectorizer
+import re
 
 corpus = ["nep-np_web_2016_100K/nep-np_web_2016_100K-sentences.txt"]
 
-vectorizer = CountVectorizer(input="filename", analyzer='char_wb', ngram_range=(1, 3))
+vectorizer = CountVectorizer(input="filename", analyzer='char_wb', ngram_range=(1, 3), 
+                             preprocessor=lambda x: re.sub(r'[^\u0900-\u097F]', '', x))
 X = vectorizer.fit_transform(corpus)
 ngrams = vectorizer.get_feature_names_out()
 counts = X.toarray().sum(axis=0)
 
 with open("nep-np_web_2016_100K/nep-np_web_2016_100K-ngram.tsv", "w", encoding="utf-8") as f:
     for e in zip(ngrams, counts):
-        f.write('\t'.join([e[0], str(int(e[1]))]) + '\n')
+        # limit to ngrams of interest - all unigrams
+        # bigrams that end with halant, 
+        # trigrams are conjunts 
+        if len(e[0]) == 1 or e[0][1] == '्': 
+            f.write('\t'.join([e[0], str(int(e[1]))]) + '\n')
